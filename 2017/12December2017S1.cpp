6366d10e9eb794d5b617aeb277b6d5ab96e2d713
Simon Jackle James <hailfallkompany@gmail.com>
2017-12-07 08:20:05 +0700

    Weekly Updates

---
+++ 2017/12December2017S1.cpp
@@ -0,0 +1,16 @@
+ รอบศุกร์ที่ 1 ธันวาคม 2017
+
++ Borisut
+https://www.facebook.com/groups/235998536871965/permalink/347534522385032/
+https://www.facebook.com/groups/235998536871965/permalink/348803612258123/
+
++ Kittipong
+https://www.facebook.com/groups/235998536871965/permalink/347625042375980/
+
++ Twentysixkeys
+https://www.facebook.com/groups/235998536871965/permalink/347958115676006/
+
++ Pollapee
+https://www.facebook.com/groups/235998536871965/permalink/348633252275159/
+
+----------
